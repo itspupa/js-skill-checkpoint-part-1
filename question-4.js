07db7afc103d48b251a2a717349ba63f581aa854
Anuna <anuaq20@gmail.com>
2025-07-23 15:17:20 +0700

    feat: add functionality to find and log the item with the lowest quantity in inventory

--- question-4.js
+++ question-4.js
@@ -5,3 +5,13 @@ const inventory = [
   { name: "Orange", price: 30, quantity: 60 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+let lowestItem = inventory[0];
+
+for (let i = 1; i < inventory.length; i++) {
+  if (inventory[i].quantity < lowestItem.quantity) {
+    lowestItem = inventory[i];
+  }
+}
+
+console.log(`สินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้าคือ ${lowestItem.name} ซึ่งมี ${lowestItem.quantity} ชิ้น`);
+
